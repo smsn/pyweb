e72f593269c0a32c1737b6c07d6e65f1b0034e12
smsn <rootsmsn@gmail.com>
2019-06-04 16:22:01 +0800

    add js and completed the user registration and login feature.

--- www/handlers.py
+++ www/handlers.py
@@ -105,7 +105,8 @@ async def cookie2user(cookie_str):
 @get('/')
 async def index(request):
     blogs = await Blog.find_all()
-    return {"__template__": "blogs.html", "blogs": blogs}
+    user = request.user
+    return {"__template__": "blogs.html", "blogs": blogs, "user": user}
 
 
 @get('/register')
@@ -189,16 +190,17 @@ async def api_register_user(*, email, name, password):
 
 
 @get(r'/test{tail:.*}')
-async def test(request):
+async def test(*, request, **kw):
     # for i in range(200):
     #     user = User(name='user%d' % i, email='user%d@example.com' % i, password='user%dpass' % i, avatar='user%dimg' % i)
     #     await user.save()  # user6.save() 仅仅是创建了一个协程, 要用await
-    users_n = await User.find_count("name")
-    print(users_n)
-    users_1n = await User.find_count(where="name like ?", args=['user10%'])
-    print(users_1n)
-    users = await User.find_all(order_by="name desc", limit=(1, 10))
-    print(len(users))
-    user = await User.find_by_pri_key(users[1].id)
-    print(user)
-    return {"__template__": "users.html", "users": users, "user": user}
+    # users_n = await User.find_count("name")
+    # print(users_n)
+    # users_1n = await User.find_count(where="name like ?", args=['user10%'])
+    # print(users_1n)
+    # users = await User.find_all(order_by="name desc", limit=(1, 10))
+    # print(len(users))
+    # user = await User.find_by_pri_key(users[1].id)
+    # print(user)
+    user = request.user
+    return {"__template__": "test.html", "user": user}
